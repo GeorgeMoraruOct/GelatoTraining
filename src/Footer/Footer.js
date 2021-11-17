11ee5356fb4d44074698b7df705d2ad1caec9f49
RalucaBibire1 <rbibire@deloittece.com>
2021-11-17 16:53:42 +0200

    Add footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -9,15 +9,34 @@ function Footer() {
         <div className="footerBody">
             <div className="info">
                 <p className="footerTitle">Useful info</p>
+                <p className="footerItem">Privacy Policy</p>
+                <p className="footerItem">Terms & Conditions</p>
+                <p className="footerItem">Cookie Policy</p>
+                <p className="footerItem">FAQs</p>
             </div>
             <div className="findUs">
                 <p className="footerTitle">You can find us in</p>
+                <p className="footerItem">London, UK</p>
+                <p className="footerItem">Bucharest, RO</p>
+                <p className="footerItem">Paris, FR</p>
+                <p className="footerItem">Sofia, BG</p>
             </div>
             <div className="about">
                 <p className="footerTitle">About gelato & donuts</p>
+                <p className="footerItem">About us</p>
+                <p className="footerItem">Store locator</p>
+                <p className="footerItem">Franchise</p>
+                <p className="footerItem">Careers</p>
+                <p className="footerItem">Contact us</p>
             </div>
             <div className="newsletter">
                 <p className="footerTitle">Newsletter</p>
+                <div class="subscribe">
+                  <input type="text" className="email" placeholder=""/>
+                  <div class="input-group-append">
+                    <button className="subscribeBtn" type="submit">Subscribe</button>  
+                  </div>
+                </div>
             </div>
         </div>
         <div className="copyright">
